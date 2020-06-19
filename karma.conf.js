4fcad259e6cd690081eedbf9b9a5bea98e909a22
Varun Mehta <mehtav23@gmail.com>
2020-06-19 10:34:51 +0530

    added unit testcases of gaurd and reporting tool

--- karma.conf.js
+++ karma.conf.js
@@ -10,23 +10,48 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-istanbul-threshold'),
+      require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
+      captureConsole: false,
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/myWebApp'),
+      dir: require('path').join(__dirname, './coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
+    },
+    istanbulThresholdReporter: {
+      src: 'coverage/coverage-final.json',
+        reporters: ['text'],
+        thresholds: {
+          global: {
+            statements: 70,
+            branches: 60,
+            lines: 70,
+            functions: 70,
+        },
+      }
+    },
+    junitReporter: {
+      outputFile: __dirname + '/../test-results.xml'
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    browserNoActivityTimeout: 2000000
   });
 };
--- src/app/core/guards/auth.guard.spec.ts
+++ src/app/core/guards/auth.guard.spec.ts
@@ -1,22 +1,39 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
 import { AuthenticateService } from '../../shared/services/authenticate.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
+import {of} from 'rxjs';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
-
+  let authenticateService: AuthenticateService;
+  const routeMock: any = { snapshot: {}};
+  const routeStateMock: any = { snapshot: {}, url: '/cookies'};
+  const routerMock = {navigate: jasmine.createSpy('navigate')};
   beforeEach(() => {
-    TestBed.configureTestingModule({
+     TestBed.configureTestingModule({
       imports: [RouterTestingModule, HttpClientModule],
       providers: [AuthenticateService]
     });
-    guard = TestBed.inject(AuthGuard);
+     guard = TestBed.get(AuthGuard);
+  });
+
+  beforeEach(() => {
+    authenticateService = TestBed.get(AuthenticateService);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  it('should be return true when user is not authenticated', () => {
+    authenticateService.userAuthenticated = false;
+    expect(guard.canActivate(routeMock, routeStateMock)).toBe(false);
+  });
+  it('should be return true when user is authenticated', () => {
+    authenticateService.userAuthenticated = true;
+    expect(guard.canActivate(routeMock, routeStateMock)).toBe(true);
   });
 });
